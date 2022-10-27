631b21ea6f9f816816d556a352db9aa48e56c4a8
sakwah <msakwahm@gmail.com>
2022-10-27 10:52:49 +0300

    added artiste attributes

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,9 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
 
 class Artiste(models.Model):
-    pass
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField
 class Song(models.Model):
     pass
 class Lyric(models.Model):
